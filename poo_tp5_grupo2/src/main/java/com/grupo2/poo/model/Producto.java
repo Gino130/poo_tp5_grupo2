5b97890b8fb1570fd8dcc7dea3107100f9878991
Gino130 <ginolombardo405@gmail.com>
2026-09-17 10:04:18 -0300

    Implemento las clases  Producto e ItemCarrito

---
+++ poo_tp5_grupo2/src/main/java/com/grupo2/poo/model/Producto.java
@@ -0,0 +1,58 @@
+package com.grupo2.poo.model;
+
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private boolean activo;
+
+    public Producto(String codigo, String nombre, double precio, boolean activo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.activo = activo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", activo=" + activo +
+                '}';
+    }
+}
